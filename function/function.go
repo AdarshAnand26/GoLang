4c17a56671e8d13403ddeba255e0f9aba918bb3b
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-08-03 23:44:20 +0530

    Chagnge it

--- function/function.go
+++ function/function.go
@@ -2,11 +2,13 @@ package main
 
 import "fmt"
 
-func add( a, b int) int{
-	return a+b
-}
+// func add( a, b int) int{
+// 	return a+b
+// }
+
+
 
 func main(){
-result:=add(3,5)
-fmt.Println(result)
+// result:=add(3,5)
+// fmt.Println(result)
 }
