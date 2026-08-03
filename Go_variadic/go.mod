2d288b877ff0f8149b3f54afd28ef61262e16f2a
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-08-03 23:51:21 +0530

    change it

---
+++ Go_variadic/go.mod
@@ -0,0 +1,3 @@
+module go_variadic.go
+
+go 1.26.5
--- function/function.go
+++ function/function.go
@@ -2,13 +2,12 @@ package main
 
 import "fmt"
 
-// func add( a, b int) int{
-// 	return a+b
-// }
+func add( a, b int) int{
+	return a+b
+}
 
 
 
-func main(){
-// result:=add(3,5)
-// fmt.Println(result)
+func main(){result:=add(3,5)
+fmt.Println(result)
 }
