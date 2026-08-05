1a52d44594b89642d8fc4f5d86dcbf47f0852cbf
Adarsh Anand <adarsh.ad91622@gmail.com>
2026-08-05 23:13:00 +0530

    10pm

--- Gen-erics/gen.go
+++ Gen-erics/gen.go
@@ -1,5 +1,60 @@
 package main 
 
+import "fmt"
+
+// func printslice(items[] int){
+// 	for _, item:=range items{
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func printStringSlice(items[] string){
+// 	for _, item:=range items{
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func main(){
+// 	nums:=[]int{1,2,3}
+// 	printslice(nums)
+// 	S_data:=[]string{"Suru, Adarsh"}
+// 	printStringSlice(S_data)
+// } 
+//Above code repeat the same code just with different datatype, we use generics to overcome it.
+
+// func printslice[T any](items[] T){
+// 	for _, item:=range items{
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func printslices[T interface{}](items[] T){
+// 	for _, item:=range items{
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func printStringSlice[T int| string | bool ](items[] T){
+// 	for _, item:=range items{
+// 		fmt.Println(item)
+// 	}
+// }
+
+// func main(){
+// 	nums:=[]string{"musu"}
+// 	//printslice(nums)
+// 	printslices(nums)
+// 	S_data:=[]int{1,2,3,4}
+// 	printStringSlice(S_data)
+// } // this is way we can use generice in go , in which we can assign any datatype or all the datatype
+
+//we can also use generic in struct
+type stack[T any] struct{
+	element []T
+} 
 func main(){
-	
+	myStack:=stack[string]{
+		element: [] string{"Suru"},
+	};
+	fmt.Println(myStack)
 }
---
+++ Gen-erics/go.mod
@@ -0,0 +1,3 @@
+module grn.go
+
+go 1.26.5
